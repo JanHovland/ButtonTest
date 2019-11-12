a4bdd71cf8ea3755054c1b2bcd5e821722d15d56
Jan Hovland <jan.hovland@lyse-net>
2019-11-12 11:46:28 +0100

    Initial Commit

---
+++ ButtonTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButtonTest
+//
+//  Created by Jan Hovland on 12/11/2019.
+//  Copyright © 2019 Jan Hovland. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
